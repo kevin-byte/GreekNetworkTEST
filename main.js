c910d084e873dac6d9b8ded80f1203054916ec0c
Holy “kevin <kevinlinarea@gmail.com>
2021-03-22 16:54:00 +0000

    new main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
 });
 
-client.login('ODIzNDIxODQ1OTMxNDI1ODAz.YFglWg.h3bANkuvokXiyg8DPVHeUD_yQyI');
+client.login(token);
